e5c59e7df787f7611087e5c2f4c2b87ac6899736
Henrik Bredahl Hansen <hbh@cadana.dk>
2026-07-09 14:07:39 +0200

    save

---
+++ RpgTrade.Web/Models/ItemSearchResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RpgTrade.Web.Models
+{
+    public class ItemSearchResponseDto
+    {
+        public List<ItemDto> Items { get; set; } = [];
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
