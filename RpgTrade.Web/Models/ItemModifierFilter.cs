d78df1bf93c0235593d78a43888be13c0be21a88
Henrik Bredahl Hansen <hbh@cadana.dk>
2026-07-09 13:32:17 +0200

    todo: bugfix min & max value not being sent to api on item fetch call

--- RpgTrade.Web/Models/ItemModifierFilter.cs
+++ RpgTrade.Web/Models/ItemModifierFilter.cs
@@ -3,8 +3,8 @@
     public class ItemModifierFilter
     {
         public int ModifierDefinitionId { get; set; }
-        public string Name { get; set; } = "";
-        public double? Min { get; set; }
-        public double? Max { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int? Min { get; set; }
+        public int? Max { get; set; }
     }
 }
